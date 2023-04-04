76122f904f020ec6bc443065a8bb6f2823509e49
GeorgeKusenkov <g-jazzman@yandex.ru>
2023-04-04 13:07:36 +0500

    rows

--- app/src/main/java/com/example/jetpack_compose_ssan_1/MainActivity.kt
+++ app/src/main/java/com/example/jetpack_compose_ssan_1/MainActivity.kt
@@ -3,14 +3,25 @@ package com.example.jetpack_compose_ssan_1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.ColorScheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.jetpack_compose_ssan_1.ui.theme.Jetpackcomposessan1Theme
 import com.example.jetpack_compose_ssan_1.ui.theme.Typography
 
@@ -19,15 +30,15 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             Jetpackcomposessan1Theme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                Row(
+                    modifier = Modifier
+                        .height(500.dp)
+                        .width(500.dp)
+                        .background(Color.LightGray),
+                    horizontalArrangement = Arrangement.Start,
                 ) {
-                    Column() {
-                        Greeting("Android")
-                        CustomText(text = "Hello, dude!")
-                    }
+                    CustomItem(weight = 3f, color = MaterialTheme.colorScheme.secondary)
+                    CustomItem(weight = 1f)
                 }
             }
         }
@@ -35,28 +46,28 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun RowScope.CustomItem(weight: Float, color: Color = MaterialTheme.colorScheme.primary) {
+    Surface(
+        modifier = Modifier
+            .width(50.dp)
+            .height(100.dp)
+            .weight(weight),
+        color = color) {
 
-@Composable
-fun CustomText(text: String) {
-    Text(
-        text = text,
-        style = Typography.titleLarge
-    )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     Jetpackcomposessan1Theme {
-        Column() {
-            Greeting("Android")
-            CustomText(text = "Hello, dude!")
+        Row(
+            modifier = Modifier.fillMaxSize(),
+            horizontalArrangement = Arrangement.Start,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            CustomItem(weight = 3f, color = MaterialTheme.colorScheme.secondary)
+            CustomItem(weight = 1f)
         }
     }
 }
