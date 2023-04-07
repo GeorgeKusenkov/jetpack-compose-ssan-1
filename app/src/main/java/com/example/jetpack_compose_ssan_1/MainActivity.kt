5ff3a49097143196b9477772e67a5c75d8a9f19a
GeorgeKusenkov <g-jazzman@yandex.ru>
2023-04-07 11:41:03 +0500

    Text Fields

--- app/src/main/java/com/example/jetpack_compose_ssan_1/MainActivity.kt
+++ app/src/main/java/com/example/jetpack_compose_ssan_1/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.jetpack_compose_ssan_1
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
@@ -10,17 +11,31 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.Email
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.style.BaselineShift
 import androidx.compose.ui.text.style.TextIndent
 import androidx.compose.ui.text.withStyle
@@ -32,45 +47,67 @@ import com.example.jetpack_compose_ssan_1.ui.theme.Typography
 
 class MainActivity : ComponentActivity() {
     @ExperimentalMaterial3Api
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             Jetpackcomposessan1Theme {
-                Surface(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .padding(25.dp)
-                ) {
-                    Column {
-                        ExpandableCard(
-                            title = "My Tittle",
-                            description = "awdawdawdwad wad wad aw daw dawd awdawdawdwad aw dawd aw dawd wa dawdawd"
-                        )
-                    }
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    Greeting()
+
                 }
             }
         }
     }
 }
-
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Greeting() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        var text = remember {mutableStateOf("Type here...")}
+        OutlinedTextField(
+            value = text.value,
+            onValueChange = {text.value = it},
+//            readOnly = true,
+//            enabled = false,
+            label = { Text(text = "Title")},
+            singleLine = true,
+            maxLines = 2,
+            leadingIcon = {
+                IconButton(onClick = { /*TODO*/ }) {
+                    Icon(
+                        imageVector = Icons.Filled.Email,
+                        contentDescription = "Email Icon")
+                }
+            },
+            trailingIcon = {
+                IconButton(onClick = { /*TODO*/ }) {
+                    Icon(
+                        imageVector = Icons.Filled.Check,
+                        contentDescription = "Email Icon")
+                }
+            },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Email,
+                imeAction = ImeAction.Search
+            ),
+            keyboardActions = KeyboardActions(
+                onSearch = {
+                    Log.d("ImeAction", "Clicked")
+                }
+            )
+        )
+    }
+}
 @ExperimentalMaterial3Api
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     Jetpackcomposessan1Theme {
-        Surface(
-            modifier = Modifier
-                .width(280.dp)
-                .height(100.dp)
-                .padding(25.dp)
-        ) {
-            Column {
-                ExpandableCard(
-                    title = "My Tittle",
-                    description = "awdawdawdwad wad wad aw daw dawd awdawdawdwad aw dawd aw dawd wa dawdawd"
-                )
-            }
-        }
+        Greeting()
     }
 }
