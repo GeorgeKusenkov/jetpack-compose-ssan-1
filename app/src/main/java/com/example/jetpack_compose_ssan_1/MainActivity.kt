b939810ca0cad21031dd0aa41504f1cab3b49518
GeorgeKusenkov <g-jazzman@yandex.ru>
2023-04-20 17:58:33 +0500

    GoogleButton

--- app/src/main/java/com/example/jetpack_compose_ssan_1/MainActivity.kt
+++ app/src/main/java/com/example/jetpack_compose_ssan_1/MainActivity.kt
@@ -1,121 +1,95 @@
 package com.example.jetpack_compose_ssan_1
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.text.KeyboardActions
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Check
-import androidx.compose.material.icons.filled.Email
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.ColorScheme
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.ParagraphStyle
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.input.TextFieldValue
-import androidx.compose.ui.text.style.BaselineShift
-import androidx.compose.ui.text.style.TextIndent
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.jetpack_compose_ssan_1.ui.theme.Jetpackcomposessan1Theme
 import com.example.jetpack_compose_ssan_1.ui.theme.Typography
 
 class MainActivity : ComponentActivity() {
-    @ExperimentalMaterial3Api
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             Jetpackcomposessan1Theme {
-                Column(modifier = Modifier
-                    .fillMaxSize(),
+                    Column(modifier = Modifier.fillMaxSize(),
                     verticalArrangement = Arrangement.Center,
                     horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-//                    Greeting()
-                    GoogleButton(
-                        text = "Sign Up wit Google",
-                        loadingText = "Creating Account...",
-                        onClicked = {Log.d("GoogleButton", "Clicked")}
-                    )
-                }
+                    ) {
+                        GoogleButton {
+
+                        }
+                    }
             }
         }
     }
 }
-@OptIn(ExperimentalMaterial3Api::class)
+
 @Composable
 fun Greeting() {
-    Column(
-        modifier = Modifier
-            .fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
     ) {
-        var text = remember {mutableStateOf("Type here...")}
-        OutlinedTextField(
-            value = text.value,
-            onValueChange = {text.value = it},
-//            readOnly = true,
-//            enabled = false,
-            label = { Text(text = "Title")},
-            singleLine = true,
-            maxLines = 2,
-            leadingIcon = {
-                IconButton(onClick = { /*TODO*/ }) {
-                    Icon(
-                        imageVector = Icons.Filled.Email,
-                        contentDescription = "Email Icon")
-                }
-            },
-            trailingIcon = {
-                IconButton(onClick = { /*TODO*/ }) {
-                    Icon(
-                        imageVector = Icons.Filled.Check,
-                        contentDescription = "Email Icon")
-                }
-            },
-            keyboardOptions = KeyboardOptions(
-                keyboardType = KeyboardType.Email,
-                imeAction = ImeAction.Search
-            ),
-            keyboardActions = KeyboardActions(
-                onSearch = {
-                    Log.d("ImeAction", "Clicked")
-                }
-            )
-        )
+        Box(
+            modifier = Modifier
+                .background(Color.Blue),
+            contentAlignment = Alignment.Center
+//                .width(100.dp)
+//                .height(100.dp)
+//                .verticalScroll(rememberScrollState())
+        ) {
+            Box(modifier = Modifier
+                .height(50.dp)
+                .width(50.dp)
+                .background(Color.Green))
+            Text(text = "I love Android", fontSize = 60.sp)
+        }
     }
 }
-@ExperimentalMaterial3Api
+
+@Composable
+fun CustomText3() {
+    Text(text = "Hello world".repeat(20), maxLines = 2, overflow = TextOverflow.Ellipsis)
+}
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     Jetpackcomposessan1Theme {
-        Greeting()
+        Column(modifier = Modifier.fillMaxSize()) {
+            CustomText3()
+        }
     }
 }
