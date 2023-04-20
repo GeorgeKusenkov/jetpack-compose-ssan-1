a4eaed08296a104bba69d3250edd2d7a5bd64bb8
GeorgeKusenkov <g-jazzman@yandex.ru>
2023-04-20 19:28:29 +0500

    Coil

--- app/src/main/java/com/example/jetpack_compose_ssan_1/MainActivity.kt
+++ app/src/main/java/com/example/jetpack_compose_ssan_1/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.jetpack_compose_ssan_1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -16,6 +17,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ColorScheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -24,6 +26,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.ParagraphStyle
 import androidx.compose.ui.text.SpanStyle
@@ -36,6 +39,13 @@ import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil.annotation.ExperimentalCoilApi
+import coil.compose.ImagePainter
+import coil.compose.rememberImagePainter
+import coil.transform.BlurTransformation
+import coil.transform.CircleCropTransformation
+import coil.transform.GrayscaleTransformation
+import coil.transform.RoundedCornersTransformation
 import com.example.jetpack_compose_ssan_1.ui.theme.Jetpackcomposessan1Theme
 import com.example.jetpack_compose_ssan_1.ui.theme.Typography
 
@@ -44,52 +54,60 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             Jetpackcomposessan1Theme {
-                    Column(modifier = Modifier.fillMaxSize(),
+                Column(
+                    modifier = Modifier.fillMaxSize(),
                     verticalArrangement = Arrangement.Center,
                     horizontalAlignment = Alignment.CenterHorizontally
-                    ) {
-                        GoogleButton {
-
-                        }
-                    }
+                ) {
+                    GoogleButton {}
+                    CoilImage()
+                }
             }
         }
     }
 }
 
+@OptIn(ExperimentalCoilApi::class)
 @Composable
-fun Greeting() {
+fun CoilImage() {
     Box(
-        modifier = Modifier.fillMaxSize(),
+        modifier = Modifier
+            .height(150.dp)
+            .width(150.dp),
         contentAlignment = Alignment.Center
     ) {
-        Box(
-            modifier = Modifier
-                .background(Color.Blue),
-            contentAlignment = Alignment.Center
-//                .width(100.dp)
-//                .height(100.dp)
-//                .verticalScroll(rememberScrollState())
-        ) {
-            Box(modifier = Modifier
-                .height(50.dp)
-                .width(50.dp)
-                .background(Color.Green))
-            Text(text = "I love Android", fontSize = 60.sp)
-        }
+        val painter =
+            rememberImagePainter(
+                data = "https://avatars.githubusercontent.com/u/9024527?v=4",
+                builder = {
+                    placeholder(R.drawable.ic_launcher_background)
+                    error(R.drawable.ic_launcher_foreground)
+                    crossfade(1000)
+                    transformations(
+//                        GrayscaleTransformation(),
+//                        CircleCropTransformation(),
+//                        BlurTransformation(LocalContext.current),
+                        RoundedCornersTransformation(50f)
+                    )
+                }
+            )
+
+        val painterState = painter.state
+        Image(painter = painter, contentDescription = "Logo Image", modifier = Modifier.padding(15.dp))
+
+//        if (painterState is ImagePainter.State.Loading) {
+//            CircularProgressIndicator()
+//        }
     }
 }
 
-@Composable
-fun CustomText3() {
-    Text(text = "Hello world".repeat(20), maxLines = 2, overflow = TextOverflow.Ellipsis)
-}
+
+
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     Jetpackcomposessan1Theme {
         Column(modifier = Modifier.fillMaxSize()) {
-            CustomText3()
         }
     }
 }
