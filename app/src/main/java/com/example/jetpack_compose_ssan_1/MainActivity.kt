23e4cb6992bc9ad11523aa541a5d26d42f650d1e
GeorgeKusenkov <g-jazzman@yandex.ru>
2023-04-25 14:20:40 +0500

    char cont limit

--- app/src/main/java/com/example/jetpack_compose_ssan_1/MainActivity.kt
+++ app/src/main/java/com/example/jetpack_compose_ssan_1/MainActivity.kt
@@ -3,12 +3,20 @@ package com.example.jetpack_compose_ssan_1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.jetpack_compose_ssan_1.ui.theme.Jetpackcomposessan1Theme
@@ -19,44 +27,45 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             Jetpackcomposessan1Theme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Column() {
-                        Greeting("Android")
-                        CustomText(text = "Hello, dude!")
-                    }
+                    MyComposable()
                 }
             }
         }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun MyComposable() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        var name by remember{ mutableStateOf("") }
+        val maxChar = 10
 
-@Composable
-fun CustomText(text: String) {
-    Text(
-        text = text,
-        style = Typography.titleLarge
-    )
+        OutlinedTextField(
+            value = name,
+            onValueChange = {
+                if (it.length <= maxChar) {
+                    name = it
+                }
+               },
+            label = { Text(text = "Name") },
+            placeholder = {Text(text = "Enter your name")
+        }, maxLines = 1)
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     Jetpackcomposessan1Theme {
-        Column() {
-            Greeting("Android")
-            CustomText(text = "Hello, dude!")
-        }
+
     }
 }
