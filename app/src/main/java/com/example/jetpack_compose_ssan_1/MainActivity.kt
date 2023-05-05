8492ab2f0be9a74f255ab8cbeb7c962300f2e779
GeorgeKusenkov <g-jazzman@yandex.ru>
2023-05-05 14:18:54 +0500

    indicator value

--- app/src/main/java/com/example/jetpack_compose_ssan_1/MainActivity.kt
+++ app/src/main/java/com/example/jetpack_compose_ssan_1/MainActivity.kt
@@ -1,53 +1,63 @@
 package com.example.jetpack_compose_ssan_1
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.jetpack_compose_ssan_1.ui.theme.Jetpackcomposessan1Theme
 import com.example.jetpack_compose_ssan_1.ui.theme.Typography
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             Jetpackcomposessan1Theme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Column() {
-                        Greeting("Android")
-                        CustomText(text = "Hello, dude!")
-                    }
+                Column(modifier = Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally) {
+                    var value by remember { mutableStateOf(0) }
+
+                    CustomComponent(
+                        indicatorValue = value
+                    )
+
+                    TextField(
+                        value = value.toString(),
+                        onValueChange = {
+                            value = if(it.isNotEmpty()) {
+                                it.toInt()
+
+                            } else {
+                                0
+                            }
+
+                        },
+                        keyboardOptions = KeyboardOptions(
+                            keyboardType = KeyboardType.Number
+                        )
+                    )
                 }
             }
         }
     }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Composable
-fun CustomText(text: String) {
-    Text(
-        text = text,
-        style = Typography.titleLarge
-    )
 }
 
 @Preview(showBackground = true)
@@ -55,8 +65,7 @@ fun CustomText(text: String) {
 fun GreetingPreview() {
     Jetpackcomposessan1Theme {
         Column() {
-            Greeting("Android")
-            CustomText(text = "Hello, dude!")
+
         }
     }
 }
